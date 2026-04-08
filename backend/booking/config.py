49ef42af95c7aeeb49946e8acad6040786a235a7
TrungNghia <stevenlarry1710@gmail.com>
2026-04-08 10:03:04 +0700

    remove fallback

--- backend/booking/config.py
+++ backend/booking/config.py
@@ -3,7 +3,7 @@ import os
 DB_URL = os.environ.get("BOOKING_DB_URL")
 VNPAY_TMN_CODE = os.environ.get("VNPAY_TMN_CODE")
 VNPAY_HASH_SECRET = os.environ.get("VNPAY_HASH_SECRET")
-VNPAY_PAYMENT_URL = os.environ.get("VNPAY_PAYMENT_URL", "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html")
+VNPAY_PAYMENT_URL = os.environ.get("VNPAY_PAYMENT_URL")
 VNPAY_RETURN_URL = os.environ.get("VNPAY_RETURN_URL")  # TODO: set your public VNPay return URL
 SMTP_HOST = os.environ.get("SMTP_HOST")
 SMTP_PORT = int(os.environ.get("SMTP_PORT", "587"))
