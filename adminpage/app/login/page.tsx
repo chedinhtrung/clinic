5d5fe5eb6c4eea6664875331fdaaa4115d83cea2
steve <stevenlarry1710@gmail.com>
2026-05-01 13:51:02 +0200

    useSearchParam bug fix

--- adminpage/app/login/page.tsx
+++ adminpage/app/login/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { FormEvent, Suspense, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
-export default function LoginPage() {
+function LoginForm() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [username, setUsername] = useState("");
@@ -96,3 +96,10 @@ export default function LoginPage() {
   );
 }
 
+export default function LoginPage() {
+  return (
+    <Suspense>
+      <LoginForm />
+    </Suspense>
+  );
+}
