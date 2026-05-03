f48db7bca1fc6e2cfc499d2559410f2fb93c2e4d
steve <stevenlarry1710@gmail.com>
2026-05-03 11:30:15 +0200

    Minor changes

--- landingpage/app/booking/confirm/page.tsx
+++ landingpage/app/booking/confirm/page.tsx
@@ -96,7 +96,7 @@ function BookingConfirmContent() {
         ) : null}
         <p>
           <br></br><br></br>
-          Sau khi xác nhận thành công, bạn sẽ được chuyển hướng đến trợ lý của Bác sĩ Nghĩa để cung cấp triệu chứng và thông tin ban đầu, giúp bác sĩ chuẩn bị tốt hơn cho buổi tư vấn. <br></br>
+          Sau khi xác nhận thành công, bạn sẽ được chuyển hướng đến trợ lý của Bác sĩ Nghĩa để cung cấp triệu chứng và thông tin ban đầu, giúp bác sĩ chuẩn bị tốt hơn cho buổi tư vấn. <br></br><br></br>
           Nếu bạn không được chuyển hướng tự động, vui lòng nhấp vào liên kết trong email xác nhận hoặc liên hệ trực tiếp với phòng khám qua email coxuongkhop.bsnghia@gmail.com để được hỗ trợ ngay.
         </p>
       </section>
