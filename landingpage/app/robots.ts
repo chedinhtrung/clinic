f1fc09c9caa040bc320a3ca8187d44f027ef2007
steve <stevenlarry1710@gmail.com>
2026-05-05 15:01:43 +0200

    enable prevent scraping internal routes

--- landingpage/app/robots.ts
+++ landingpage/app/robots.ts
@@ -14,7 +14,17 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-      
+        disallow: [
+          "/booking",
+          "/booking/",
+          "/booking/*",
+          "/booking-email",
+          "/booking-email/*",
+          "/confirmation",
+          "/confirmation/*",
+          "/payment",
+          "/payment/*",
+        ],
       },
     ],
     sitemap: "https://chedinhnghia.com/sitemap.xml",
