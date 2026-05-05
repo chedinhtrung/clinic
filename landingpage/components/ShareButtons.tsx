712c0e95cda22405065bb1432fb6feaf81b5d257
steve <stevenlarry1710@gmail.com>
2026-05-05 18:00:28 +0200

    minor change

--- landingpage/components/ShareButtons.tsx
+++ landingpage/components/ShareButtons.tsx
@@ -72,9 +72,9 @@ export default function ShareButtons({ url, title, variant = "light" }: ShareBut
         aria-label="Sao chép liên kết"
         className={buttonClassName}
       >
-        <svg aria-hidden="true" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"  className="h-4 w-4">
-          <path d="M13.5442 10.4558C11.8385 8.75022 9.07316 8.75022 7.36753 10.4558L4.27922 13.5442C2.57359 15.2498 2.57359 18.0152 4.27922 19.7208C5.98485 21.4264 8.75021 21.4264 10.4558 19.7208L12 18.1766" stroke="#ffffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-          <path d="M10.4558 13.5442C12.1614 15.2498 14.9268 15.2498 16.6324 13.5442L19.7207 10.4558C21.4264 8.75021 21.4264 5.98485 19.7207 4.27922C18.0151 2.57359 15.2497 2.57359 13.5441 4.27922L12 5.82338" stroke="#faf9f9ff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <svg aria-hidden="true" viewBox="0 0 24 24" fill="none" className="h-4 w-4 stroke-current">
+          <path d="M13.5442 10.4558C11.8385 8.75022 9.07316 8.75022 7.36753 10.4558L4.27922 13.5442C2.57359 15.2498 2.57359 18.0152 4.27922 19.7208C5.98485 21.4264 8.75021 21.4264 10.4558 19.7208L12 18.1766" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M10.4558 13.5442C12.1614 15.2498 14.9268 15.2498 16.6324 13.5442L19.7207 10.4558C21.4264 8.75021 21.4264 5.98485 19.7207 4.27922C18.0151 2.57359 15.2497 2.57359 13.5441 4.27922L12 5.82338" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
       </button>
 
